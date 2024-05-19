915deb7a92ea6aa231b2ae81f3d50f978d930bf2
Weronika Tatarek <weronikatatarek@interia.pl>
2024-05-19 20:31:43 +0200

    poprawki

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 """
 This module is the entry point for the application.
 """
-from src.graph_generation import generate_maze_kruskal, draw_maze
+from src.display_maze import draw_maze
+from src.graph_generation import generate_maze_kruskal
 
 if __name__ == "__main__":
     print("Hello ACO world!")
