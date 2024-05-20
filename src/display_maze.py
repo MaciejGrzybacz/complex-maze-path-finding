83b20d7afeeb6345fa4eda2a41e9c79275977363
richardzmija <zmija.ryszard@protonmail.com>
2024-05-20 13:58:28 +0200

    Added cycles to generated mazes and cleaned up a few files to improve readability.

--- src/display_maze.py
+++ src/display_maze.py
@@ -1,7 +1,17 @@
+"""
+Module for displaying the path-finding process
+of the ACO algorithm.
+
+This module provides functions for displaying the maze together
+with the ants exploring it trying to find the shortest path.
+"""
+
 import networkx as nx
 import pygame
 
 
+# TODO: Fix the maze display so that it considers the edges
+# in the graph as paths between cells and not walls.
 def draw_maze(mst: nx.Graph, rows: int, cols: int, cell_size: int = 20):
     pygame.init()
     width, height = cols * cell_size, rows * cell_size
